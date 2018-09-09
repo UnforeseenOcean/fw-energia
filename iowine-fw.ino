0d0f200ce23cbe9cc58bc7284338ad09c9c5bf0e
Andrejus <andrejus@think-engineer.com>
2018-09-09 18:23:26 +0100

    Implement HTTP POST to Firebase

--- iowine-fw.ino
+++ iowine-fw.ino
@@ -27,6 +27,43 @@ void startWiFi() {
     printWifiStatus();
 }
 
+void blinkLED() {
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
+}
+
+void postData() {
+    client.stop();
+
+    String data = "{\"device\":\"test\",\"temperature\":21,\"humidity\":21,\"foo\":\"bar\"}";
+
+    if (client.connect("us-central1-iowine-cloud.cloudfunctions.net", 80)) {
+        Serial.println("Sending HTTP POST Request...");
+
+        client.println("PUT /pushData HTTP/1.1");
+        client.println("Host: us-central1-iowine-cloud.cloudfunctions.net");
+        client.println("User-Agent: Energia/1.1");
+        client.print("Content-Length: ");
+
+        // calculate the length of the sensor reading in bytes:
+        // 8 bytes for "sensor1," + number of digits of the data:
+        int thisLength = data.length();
+        client.println(thisLength);
+
+        // last pieces of the HTTP PUT request:
+        client.println("Accept: text/plain");
+        client.println("Content-Type: application/json");
+        client.println("Connection: close");
+        client.println();
+
+        client.print(data);
+    } else {
+        Serial.println("Failed to send HTTP POST Request");
+    }
+}
+
 void setup() {
     /* Serial */
     Serial.begin(115200);
@@ -37,9 +74,8 @@ void setup() {
 }
 
 void loop() {
-    digitalWrite(LED, HIGH);
-    delay(100);
-    digitalWrite(LED, LOW);
-    delay(100);
+    blinkLED();
+    postData();
+    delay(5000);
 }
 
