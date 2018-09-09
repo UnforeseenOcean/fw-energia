ab74a56d9189ab985c09d7d4ea2d11926aca44f2
Andrejus <andrejus@think-engineer.com>
2018-09-09 17:49:20 +0100

    Connect to Wi-Fi

--- iowine-fw.ino
+++ iowine-fw.ino
@@ -1,7 +1,37 @@
 #define LED RED_LED
 
+#include <SPI.h>
+#include <WiFi.h>
+
+char ssid[] = "iowine-foo";
+char pass[] = "iowine-bar";
+
+WiFiClient client;
+
+/// https://github.com/energia/Energia/blob/master/libraries/SimplelinkWifi/examples/ThingSpeakClient/ThingSpeakClient.ino
+void printWifiStatus() {
+    Serial.println("SSID: " + String(WiFi.SSID()));
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+}
+void startWiFi() {
+    /* Kill existing connection */
+    WiFi.disconnect();
+    client.stop();
+    /* Start new connection */
+    WiFi.begin(ssid, pass);
+    /* Wait for WiFi */
+    delay(2500);
+    printWifiStatus();
+}
+
 void setup() {
+    /* Serial */
+    Serial.begin(115200);
+    /* Debug pins */
     pinMode(LED, OUTPUT);
+    /* WiFi */
+    startWiFi();
 }
 
 void loop() {
@@ -10,3 +40,4 @@ void loop() {
     digitalWrite(LED, LOW);
     delay(100);
 }
+
